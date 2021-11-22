3ce576dbd48284278a31a94abd76a7b48ca8d3ad
T1xAn <pyps47.pyps47@yandex.ru>
2021-11-23 02:11:13 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaahhhhh......

--- lab8/Source.cpp
+++ lab8/Source.cpp
@@ -52,6 +52,17 @@ void rad_dum_wasteland( int* ext, int size, int** distG ) {
 	cout << endl << endl << "Диаметр графа равен  " << dum;
 }
 
+void rad_dum_wasteland_v2(int* ext, int size) {
+	for (int i = 0; i < size; i++) {
+		if (ext[i] > dum)
+			dum = ext[i];
+		if (ext[i] < rad)
+			rad = ext[i];
+	}
+	cout << endl << endl << "Радиус графа равен  " << rad;
+	cout << endl << endl << "Диаметр графа равен  " << dum;
+}
+
 void best_wateland_survivalists( int* ext, int size) {
 	cout << endl << " Подмножество центральных вершин:  ";
 	for (int i = 0; i < size; i++)
@@ -62,6 +73,7 @@ void best_wateland_survivalists( int* ext, int size) {
 	for (int i = 0; i < size; i++)
 		if (ext[i] == dum)
 			cout << "  " << i;
+	cout << endl;
 }
 
 void step_vrshi_search_v1(int size, int** arr) {
@@ -95,6 +107,26 @@ void step_vrshi_search_v1(int size, int** arr) {
 			cout << "  " << i;
 }
 
+void bfs(int** arr_inc, int num, int* dist, int size, int edge_count) {
+	queue <int> que;
+	dist[num] = 0;
+	que.push(num);
+	while (!que.empty()) {
+		num = que.front();
+		que.pop();
+		for (int i = 0; i < edge_count; i++) {
+			if (arr_inc[num][i] > 0) {
+				for (int count = 0; count < size; count++) {
+					if (arr_inc[count][i] > 0 && dist[count] > dist[num] + arr_inc[count][i]) {
+						que.push(count);
+						dist[count] = dist[num] + arr_inc[count][i];
+					}
+				}
+			}
+		}
+	}
+}
+
 void main() {
 	srand(time(0));
 	setlocale(LC_ALL, "Russian");
@@ -111,7 +143,7 @@ void main() {
 		arr[row][row] = 0;
 		for (int col = row + 1; col < size; col++) {
 			arr[row][col] = rand() % 100;
-			if (arr[row][col] < 70) {
+			if (arr[row][col] < 40) {
 				arr[row][col] = 0;
 				arr[col][row] = arr[row][col];
 			}
@@ -175,119 +207,18 @@ void main() {
 
 	step_vrshi_search_v1(size, arr);
 
-	system("pause");
-}
 
-/*#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <locale.h>
-#include <iostream>
-#include <stack> 
-#include <queue>
-using namespace std;
+	//////////////////////////////////////////////////////////////////////////////////
 
-int j = 0;
-int r = 0;
-int i = 0;
-int k = 0;
-
-void bfs(int** arr_inc, int num, int* dist, int size, int edge_count) {
-	queue <int> que;
-	dist[num] = 0;
-	que.push(num);
-	while (!que.empty()) {
-		num = que.front();
-		//printf("%d ", num);
-		que.pop();
-		for (int i = 0; i < edge_count; i++) {
-			if (arr_inc[num][i] > 0) {
-				for (int count = 0; count < size; count++) {
-					if (arr_inc[count][i] > 0 && dist[count] > dist[num] + arr_inc[count][i]) {
-						que.push(count);
-						dist[count] = dist[num] + arr_inc[count][i];
-					}
-				}
-			}
-		}
-	}
-}
-
-//void bfs(int current_vertex, int size, int edge_count, int** arr_inc, int* dist, int path_length) {
-//
-//	dist[current_vertex] = path_length;
-//
-//	int p;
-//
-//
-//
-//	for (i = k; i < size; i++)
-//	{
-//		j = r;
-//		if ((arr_inc[i][j] != 0) && (dist[i] > path_length + arr_inc[i][j]))
-//		{
-//			dist[i] = path_length;
-//			p = i;
-//			//cout << i << " ";
-//			for (j = 0; j < edge_count; j++)
-//			{
-//				i == p;
-//				if (arr_inc[i][j] != 0)
-//				{
-//					r = j;
-//					for (k = 0; k < size; k++)
-//					{
-//						j == r;
-//						if ((arr_inc[k][j] != 0) && (dist[k] > path_length + arr_inc[i][j]))
-//							bfs(i, size, edge_count, arr_inc, dist, path_length + arr_inc[i][j]);
-//					}
-//				}
-//			}
-//		}
-//	}
-//}
-
-
-int main() {
-	srand(time(0));
-	setlocale(LC_ALL, "Russian");
-
-	int size;
-
-	cout << "   Введите размерность матрицы: ";
-	cin >> size;
-	cout << endl;
-
-	int** arr = new int* [size];
-	for (int count = 0; count < size; ++count)
-		arr[count] = new int[size];
-
-	for (int row = 0; row < size; row++) {
-		arr[row][row] = 0;
-		for (int col = row + 1; col < size; col++) {
-			arr[row][col] = rand() % 100;
-			arr[col][row] = arr[row][col];
-		}
-	}
-	cout << "   ";
-
-	for (int row = 0; row < size; row++)
-	{
-		for (int col = 0; col < size; col++) {
-			cout << arr[row][col] << " ";
-		}
-		cout << endl << "   ";
-	}
-	cout << endl;
-////////////////////////////////////////////////////////////////////////////////
-	int edge_count=0;
+	int edge_count = 0;
 	for (int row = 0; row < size; row++) {
 		for (int col = row + 1; col < size; col++) {
 			if (arr[row][col])
 				edge_count++;
 		}
 	}
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
+
 	int** arr_inc = new int* [size];
 	for (int count = 0; count < size; ++count) {
 		arr_inc[count] = new int[edge_count];
@@ -314,10 +245,10 @@ int main() {
 	}
 
 	int* dist = new int[size];
-	int* ext = new int[size];
+	int* ext_s = new int[size];
 	int max;
 
-	cout << endl;
+	cout << endl << endl << "Эксцентриситеты вершин\n\n";
 	for (int cunt = 0; cunt < size; ++cunt) {
 		for (int count = 0; count < size; ++count)
 			dist[count] = INT_MAX;
@@ -329,12 +260,22 @@ int main() {
 		for (int fuck = 0; fuck < size; ++fuck) {
 			if (dist[fuck] == INT_MAX)
 				dist[fuck] = 0;
-			//cout << " " << dist[fuck];
-			if(dist[fuck] > max)
+			if (dist[fuck] > max)
 				max = dist[fuck];
 		}
-		ext[cunt] = max;
+		ext_s[cunt] = max;
 		cout << max << " ";
 	}
 	cout << endl;
-}*/
+
+	rad_dum_wasteland_v2(ext_s, size);
+
+	
+
+	best_wateland_survivalists(ext_s, size);
+
+	system("pause");
+
+
+}
+
